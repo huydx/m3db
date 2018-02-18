df36d46fd64f919693a112f3e4ef1dbf121a6843
Prateek Rungta <rungta@uber.com>
2018-02-17 19:37:01 -0500

    another issue

--- storage/index/iterator.go
+++ storage/index/iterator.go
@@ -96,7 +96,7 @@ func (i *idsIter) parseAndStore(d doc.Document) {
 	// TODO(prateek): add ident.Pool method for - Get([]byte) ID - do not assume ownership of input bytes
 	i.currentID = ident.StringID(string(d.ID))
 	for _, f := range d.Fields {
-		if bytes.Compare(f.Name, ReservedFieldNameNamespace) == 0 {
+		if bytes.Equal(f.Name, ReservedFieldNameNamespace) {
 			i.currentNs = ident.StringID(string(f.Value))
 			continue
 		}
